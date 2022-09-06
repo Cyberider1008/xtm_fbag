1c6d457f711f917536437959ee006653e71098fd
Cyberider1008 <abaranwal.it@gmail.com>
2022-09-06 21:32:58 +0530

    remove all files

--- xtm_fbag_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class XtmFbagAppConfig(AppConfig):
-    '''---------XTM FBAG APP configure here---------'''
-
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'xtm_fbag_app'
